8024973c6ee1b8c21e39f011b9c127cbb58e9044
Sai Krishna Mohan <m.saikrishna.msk@gmail.com>
2026-03-11 17:30:57 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Print("$ ")
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input %w", err)
+	}
+	input = strings.TrimSpace(input)
+	fmt.Println(input)
 }
